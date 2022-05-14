d4afcf531d6814aa1968f53a7f1e20303b5fed67
TGWolf <60626007+CMolitoris@users.noreply.github.com>
2022-05-13 23:01:33 -0400

    Converted accordion to cards, Added styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import { useState, useEffect } from 'react';
-import { Accordion } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
 import axios from 'axios';
+import hoth from "./static/images/hoth.jpg";
 
 function App() {
 
@@ -22,31 +23,52 @@ function App() {
   }
 
   return (
-    <div >
-      <h2 class='title'>
-        Star Wars Planets
-      </h2>
-      <div class='acc-style'>
-          <Accordion style={{textAlign: 'left'}} id="styled-acc">
-            {planets.map((planet, index) => {
-                return (
-                  <Accordion.Item eventKey={index}>
-                    <Accordion.Header >{planet.name}</Accordion.Header>
-                    <Accordion.Body >
-                      Rotation Period: {planet.rotation_period}<br/>
-                      Orbital Period: {planet.orbital_period}<br/>
-                      Diameter: {planet.diameter}<br/>
-                      Climate: {planet.climate}<br/>
-                      Gravity: {planet.gravity}<br/>
-                      Terrain: {planet.terrain}<br/>
-                      Surface Water: {planet.surface_water}<br/>
-                      Population: {planet.population}
-                    </Accordion.Body>
-                  </Accordion.Item>
-                )
-            })}
-          </Accordion>
+    <div className='main-container'>
+      <div className='container con-title'>
+        <div className='row'>
+          <div className='offset-1 col-10'>
+            <h2>Star Wars Planets</h2>
+            <hr/>
+            <h6 className='text-center'>
+              Below you will find your favorite Star Wars planets. When toggling the drop-down
+              you will be able to see the details and find a button which will give you a more 
+              detailed view of each.
+            </h6>
+          </div>
+        </div>
       </div>
+      
+      <div className='container card-body-style' align="center">
+        <div className='row  '>                            
+          {planets.map((planet,i) => {
+            return (
+                <div className='col-4  '>
+                    <Card className='mt-2 ' key={i} style={{ width: 'auto' }}>
+                        <Card.Img className='card-image'  src={hoth} />
+                        <Card.Body>
+                            <Card.Title>{planet.name}</Card.Title>
+                                <div>
+                                    <hr/>
+                                    <div className='card-scroll'>
+                                      Rotation Period: {planet.rotation_period}<br/>
+                                      Orbital Period: {planet.orbital_period}<br/>
+                                      Diameter: {planet.diameter}<br/>
+                                      Climate: {planet.climate}<br/>
+                                      Gravity: {planet.gravity}<br/>
+                                      Terrain: {planet.terrain}<br/>
+                                      Surface Water: {planet.surface_water}<br/>
+                                      Population: {planet.population}
+                                    </div>
+                                </div>
+                            <Button id='button-color' className='w-100' variant="dark">Details <i class="bi bi-link"></i></Button>
+                        </Card.Body>
+                    </Card>
+                </div>
+            )}
+          )}
+        </div>
+      </div>
+                    
     </div>
   );
   
